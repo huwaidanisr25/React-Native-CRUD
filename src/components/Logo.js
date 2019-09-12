1b810c71a6eef547b3830a14b484ab553bd9e7dc
huwaidanisrina <danishuwaida@gmail.com>
2019-09-12 07:36:52 +0700

    Login & Register

---
+++ src/components/Logo.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import {
+
+StyleSheet,
+
+Text,
+
+View,
+
+Image
+
+} from 'react-native';
+
+
+
+export default class Logo extends Component<{}> {
+
+render(){
+
+return(
+
+<View style={styles.container}>
+
+<Image  style={{width:90, height: 90}}
+
+source={require('../images/internet.png')}/>
+
+<Text style={styles.logoText}>Welcome to My app.</Text>
+
+</View>
+
+)
+
+}
+
+}
+
+
+
+const styles = StyleSheet.create({
+
+container : {
+
+flexGrow: 1,
+
+justifyContent:'flex-end',
+
+alignItems: 'center'
+
+},
+
+logoText : {
+
+marginVertical: 15,
+
+fontSize:18,
+
+color:'rgba(255, 255, 255, 0.7)'
+
+}
+
+});
